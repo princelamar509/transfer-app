c228dc56073259a38e69265ae42c9925c656d891
JAMES M <168496401+princelamar509@users.noreply.github.com>
2024-09-28 10:52:28 -0400

    json

--- transfer-app/vite.config.js
+++ transfer-app/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/shoe-store/',
+  base: '/transfer-app/',
   plugins: [react()],
 })
