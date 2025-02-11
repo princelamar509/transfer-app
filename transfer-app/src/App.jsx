d421976d6ddb6e99ef43a57b6427c308c335d944
JAMES M <168496401+princelamar509@users.noreply.github.com>
2025-02-11 12:40:00 -0500

    RE

--- transfer-app/src/App.jsx
+++ transfer-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Preloader from './components/Preloader';
